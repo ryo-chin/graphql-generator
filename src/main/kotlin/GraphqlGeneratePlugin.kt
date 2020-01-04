741b0e530e24bcf94f70cf3f65e106c4b528fb71
ryo-chin <h031203yama@gmail.com>
2020-01-05 02:26:00 +0900

    refactor: use main function

--- src/main/kotlin/GraphqlGeneratePlugin.kt
+++ src/main/kotlin/GraphqlGeneratePlugin.kt
@@ -1,6 +1,5 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
-import java.io.InputStreamReader
 
 /**
  * @author hakiba
@@ -9,15 +8,9 @@ class GraphqlGeneratePlugin : Plugin<Project> {
     override fun apply(project: Project) {
         project.run {
             tasks.create("generate").doLast {
-                val schemaPath = "src/test/resources/graphql/schema.graphql"
-                val outputDirPath = "tmp/autogen"
-                val idType = System.getProperty("idType")?.let { IDType.valueOf(it) } ?: IDType.String
-
-                val generator = Generator(idType)
-
-                generator.read(schemaPath)
-                        .flatMap { generator.parse(InputStreamReader(it.inputStream()).buffered().readText()) }
-                        .forEach { generator.generate(outputDirPath, it) }
+                val inputPath = "src/test/resources/graphql/schema.graphql"
+                val outputPath = "tmp/autogen"
+                main(arrayOf(inputPath, outputPath))
             }
         }
     }
